fc9646ce07446f8f3c7f3cb40a0665ae02b6c596
barron-paw <532394442@qq.com>
2025-11-11 14:36:07 +0800

    Delete frontend directory

--- frontend/src/api/config.js
+++
@@ -1,9 +0,0 @@
-import apiClient from './client';
-
-export function fetchMonitorConfig() {
-  return apiClient.get('/config');
-}
-
-export function updateMonitorConfig(payload) {
-  return apiClient.post('/config', payload);
-}
--- frontend/src/api/subscription.js
+++
@@ -1,5 +0,0 @@
-import apiClient from './client';
-
-export function verifySubscription(txHash) {
-  return apiClient.post('/subscription/verify', { txHash });
-}
